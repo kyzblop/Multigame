6a3784e21016866d8862af032cb55143a6c71bfa
kyzblop <jeremymoi.jd@gmail.com>
2024-11-07 11:50:34 +0100

    pas de raccourci clavier

--- Nim/js/scriptNim.js
+++ Nim/js/scriptNim.js
@@ -163,3 +163,17 @@ document.addEventListener('contextmenu', function(event) {
     event.preventDefault();
     alert("Pas de clic droit ici, Nulos")
 })
+
+// Gestion des raccourcis claviers
+document.addEventListener('keydown', function(event) {
+    // Désactiver les raccourcis pour les outils de développement
+    if ((event.ctrlKey && event.shiftKey && event.key === 'I') || // Ctrl + Shift + I
+        (event.key === 'F12') || // F12
+        (event.ctrlKey && event.key === 'U') || // Ctrl + U (voir source)
+        (event.ctrlKey && event.key === 'S') || // Ctrl + S (enregistrer la page)
+        (event.ctrlKey && event.key === 'P')) { // Ctrl + P (imprimer)
+  
+      event.preventDefault(); // Empêche l'action par défaut
+      alert('Essaie encore');
+    }
+});
